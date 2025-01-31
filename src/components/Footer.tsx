12ea46ae76ad906e40af4bb6eb48a29209b7fd8e
Deevoice <furaevnikita@gmail.com>
2025-02-01 01:05:47 +0300

    basic layout + header, footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <p className={styles.copyright}>
+                        © {currentYear} dee-dev
+                    </p>
+                    <div className={styles.socialLinks}>
+                        <Link
+                            href="https://github.com/Deevoice"
+                            target="_blank"
+                            className={styles.socialLink}
+                        >
+                            <svg className={styles.icon} viewBox="0 0 24 24">
+                                <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.92 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z" />
+                            </svg>
+                        </Link>
+                        <Link
+                            href="https://t.me/deevoice"
+                            target="_blank"
+                            className={styles.socialLink}
+                        >
+                            <svg className={styles.icon} viewBox="0 0 24 24">
+                                <path d="M19 0H5a5 5 0 00-5 5v14a5 5 0 005 5h14a5 5 0 005-5V5a5 5 0 00-5-5zM8 19H5V8h3v11zM6.5 6.732c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zM20 19h-3v-5.604c0-3.368-4-3.113-4 0V19h-3V8h3v1.765c1.396-2.586 7-2.777 7 2.476V19z" />
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
